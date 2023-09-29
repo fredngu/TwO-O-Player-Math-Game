941042250469d43ea44391d57110b6cd1072bb37
Frederik Nguyen <frederik.nguyen@outlook.com>
2023-09-28 20:48:25 -0400

    Finish gameplay loop and score check

--- game.rb
+++ game.rb
@@ -15,13 +15,29 @@ class Game
     puts "#{@player1.name}: #{@player1.lives}/3 vs #{@player2.name}: #{@player2.lives}/3 "
   end
 
+  def check_scores
+    if @player1.lives == 0 
+      puts "#{@player2.name} wins with a score of #{@player2.lives}/3"
+      puts "----- GAME OVER -----"
+      puts "Good bye!"
+      exit(0)
+    elsif @player2.lives == 0
+      puts "#{@player1.name} wins with a score of #{@player1.lives}/3"
+      puts "----- GAME OVER -----"
+      puts "Good bye!"
+      exit(0)
+    end
+  end
+
   def player_turn
     @player1.ask_math
     player_scores
+    check_scores
     puts "----- NEW TURN -----"
     @player2.ask_math
     player_scores
+    check_scores
     puts "----- NEW TURN -----"
-
+    player_turn
   end
 end 
